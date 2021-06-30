6cded7601126b2629d1ef07162136b1b74991daa
Haris Nazir <haris.nazir1963@gmail.com>
2021-06-30 15:16:03 +0100

    Added Serialiser

--- main/urls.py
+++ main/urls.py
@@ -2,9 +2,15 @@ from django.urls import path
 
 from . import views
 
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register(r'MonthlyDataUpload', views.MonthlyDataUploadViewSet)
+
 urlpatterns = [
     path('', views.GetCountryCode, name='Get Country Code'),
     path('generate/', views.GenerateExcel, name='Generate Excel'),
     path('upload/', views.UploadFile, name="Upload Excel"),
-    path('uploadscreen/',views.UploadScreen.as_view(), name="Upload Screen")
+    path('uploadscreen/',views.UploadScreen.as_view(), name="Upload Screen"),
+    path('/api', include('rest_framework', namespace='rest_framework')),
 ]
